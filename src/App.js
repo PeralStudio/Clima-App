cb6a29637402e3f57c987749155fe6e4e51b5eb4
PeralStudio <info@peralstudio.com>
2020-06-08 23:42:11 +0200

    Clima App React

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, {Fragment, useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
+
+
+function App() {
+
+      // state del formulario
+
+      const [busqueda, guardarBusqueda] = useState({
+        ciudad: '',
+        pais:''
+        
+    });
+
+    const [consultar, guardarConsultar] = useState (false);
+    const [resultado, guardarResultado] = useState ({});
+    const [error, guardarError] = useState(false);
+
+    const {ciudad, pais} = busqueda;
+
+    useEffect(() => {
+      const consultarAPI = async () => {
+
+        if(consultar) {
+          const appId = '40efccd434eefd0344923485b60fbda7';
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+
+          guardarResultado(resultado);
+          guardarConsultar(false);
+
+          //Detecta si hay resultados correctos en la consulta
+
+          if(resultado.cod === "404") {
+            guardarError(true);
+          }else {
+             guardarError(false);
+          }
+        }
+        
+
+      }
+      consultarAPI();
+
+    // eslint-disable-next-line
+    },[consultar]);
+
+    let componente;
+    if (error) {
+      componente = <Error mensaje="No hay resultados" />
+    } else {
+      componente = <Clima resultado= {resultado} />
+    }
+
+    
+
+  return (
+    <Fragment>
+      <Header
+        titulo='Clima React Alberto Peral'
+      />
+
+      <div className="contenedor-form">
+            <div className="container">
+              <div className="row">
+                <div className="col m6 s12">
+                  <Formulario
+                    busqueda={busqueda}
+                    guardarBusqueda={guardarBusqueda}
+                    guardarConsultar={guardarConsultar}
+                  />
+                </div>
+                <div className="col m6 s12">
+                  {componente}
+                </div>
+
+              </div>
+            </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Clima.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Clima =({resultado}) => {
+
+    //extraer los avlores
+    const { name, main}= resultado;
+
+    if (!name) return(false);
+
+    //grados kelvin
+    const kelvin = 273.15;
+
+
+    return (
+
+        <div className="card-panel white col s12">
+            <h2>El clima de {name} es: </h2>
+            <p className="temperatura">
+                {parseFloat (main.temp - kelvin, 10).toFixed(1)}<span>&#x2103;</span>
+            </p>
+            <p>Temperatura Máxima:
+                {parseFloat (main.temp_max - kelvin, 10).toFixed(1)} <span>&#x2103;</span>
+            </p>
+            <p>Temperatura Minima:
+                {parseFloat (main.temp_min - kelvin, 10).toFixed(1)} <span> &#x2103;</span>
+            </p>
+        </div>
+    )
+}
+
+Clima.ropTypes = {
+    resultado: PropTypes.object.isRequired
+}
+
+export default Clima;
