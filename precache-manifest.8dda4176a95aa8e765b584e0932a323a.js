83acb1d65a3101ee6e5484a2f113beeeeaefceb3
PeralStudio <info@peralstudio.com>
2020-06-10 06:28:04 +0200

    Updates

---
+++ precache-manifest.8dda4176a95aa8e765b584e0932a323a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d1ffe6aa378e466e1ef796018c363f",
+    "url": "/Clima-App/index.html"
+  },
+  {
+    "revision": "f303e62d282928b0ffc8",
+    "url": "/Clima-App/static/css/main.80bc532b.chunk.css"
+  },
+  {
+    "revision": "564a2b1d55429e9c47c1",
+    "url": "/Clima-App/static/js/2.1255aeb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clima-App/static/js/2.1255aeb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f303e62d282928b0ffc8",
+    "url": "/Clima-App/static/js/main.965d44dd.chunk.js"
+  },
+  {
+    "revision": "a01aeaf070735ff5b3f7",
+    "url": "/Clima-App/static/js/runtime-main.c39de707.js"
+  }
+]);
