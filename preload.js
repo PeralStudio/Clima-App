6dcbd6dffefbd3fd9e571a8b075b3ef6fc9a65cb
Alberto <info@peralstudio.com>
2023-01-07 17:24:26 +0100

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-    installApps: (apps) => ipcRenderer.send("request-mainprocess-action", apps),
-});
